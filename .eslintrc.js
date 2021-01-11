76b35bc2879ae8c9a452fcb5207a526f275d4513
Alexey Litovchenko <alex.lit@outlook.com>
2021-01-11 10:18:34 +0300

    refactor: update main config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,4 @@
 module.exports = {
-  extends: [
-    require.resolve("./configs/nuxt.js"),
-    require.resolve("./configs/import.js"),
-    require.resolve("./configs/simple-import-sort.js"),
-    require.resolve("./configs/jsdoc.js"),
-    require.resolve("./configs/sonar.js"),
-    require.resolve("./configs/typescript.js"),
-    require.resolve("./configs/vue.js"),
-    require.resolve("./configs/prettier.js"),
-  ],
-
   root: true,
 
   env: {
@@ -25,4 +14,20 @@ module.exports = {
     "**/node_modules/**",
     "**/sw.js",
   ],
+
+  extends: [
+    require.resolve("./configs/nuxt.js"),
+    require.resolve("./configs/import.js"),
+    require.resolve("./configs/simple-import-sort.js"),
+    require.resolve("./configs/jsdoc.js"),
+    require.resolve("./configs/sonar.js"),
+    require.resolve("./configs/typescript.js"),
+    require.resolve("./configs/vue.js"),
+    require.resolve("./configs/prettier.js"),
+  ],
+
+  rules: {
+    "no-console": "off",
+    "no-use-before-define": "off",
+  },
 };
--- configs/nuxt.js
+++ configs/nuxt.js
@@ -1,8 +1,3 @@
 module.exports = {
   extends: ["@nuxtjs/eslint-config-typescript", "plugin:nuxt/recommended"],
-
-  rules: {
-    "no-console": "off",
-    "no-use-before-define": "off",
-  },
 };
