d8373d3e7cc8454761ff65e74b699583243df874
Alexey Litovchenko <alex.lit@outlook.com>
2021-01-20 12:34:45 +0300

    feat: add sql plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,7 @@ module.exports = {
     require.resolve('./configs/no-loops.js'),
     require.resolve('./configs/no-secrets.js'),
     require.resolve('./configs/filenames.js'),
+    require.resolve('./configs/sql.js'),
     require.resolve('./configs/prettier.js'),
 
     /**
--- test/type-script.comp.dsf.ts
+++ test/type-script.comp.dsf.ts
@@ -1,14 +1,27 @@
 const Auth = (args: any) => args;
+const sql = (...args: any) => args;
 
 const arr = [1, 2, 3];
 
 arr.forEach((item) => item);
 
 const BASE64_CHARS = '1+/=';
+
+const a = `
+SELECT
+  ${'foo'}
+FROM
+  ${'bar'}
+`;
+
+const b = sql`
+SELECT
+  1
+`;
 export default class typeScript {
-  @Auth({ sdf: BASE64_CHARS })
+  @Auth({ sdf: BASE64_CHARS, b })
   _selectedUserId;
 
   @Auth('user')
-  user;
+  user = a;
 }
