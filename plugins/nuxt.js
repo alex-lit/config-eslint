2a73145963b924b549e497a8a5daa960212b514f
Alexey Litovchenko <alex.lit@outlook.com>
2021-01-29 18:51:35 +0300

    chore: declare nuxt plugin

--- plugins/nuxt.js
+++ plugins/nuxt.js
@@ -2,5 +2,7 @@
  * @see [nuxt](https://github.com/nuxt/eslint-config/tree/master/packages/eslint-config)
  */
 module.exports = {
+  plugins: ['nuxt'],
+
   extends: ['@nuxtjs/eslint-config-typescript', 'plugin:nuxt/recommended'],
 };
