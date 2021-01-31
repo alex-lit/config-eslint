a8b4941f7fed51c4cefa9c3f4a4422c1c4114791
Alexey Litovchenko <alex.lit@outlook.com>
2021-01-31 16:00:59 +0300

    chore: update deps

--- plugins/nuxt.js
+++ plugins/nuxt.js
@@ -1,8 +1,12 @@
 /**
- * @see [nuxt](https://github.com/nuxt/eslint-config/tree/master/packages/eslint-config)
+ * @see [eslint-plugin-nuxt](https://github.com/nuxt/eslint-plugin-nuxt)
  */
 module.exports = {
   plugins: ['nuxt'],
 
   extends: ['@nuxtjs/eslint-config-typescript', 'plugin:nuxt/recommended'],
+
+  rules: {
+    'nuxt/require-func-head': 'error',
+  },
 };
