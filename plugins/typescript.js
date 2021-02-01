b62c1d7b23deaa9b98c0b9a9c59b3724c213d9c2
Alexey Litovchenko <alex.lit@outlook.com>
2021-02-01 14:29:35 +0300

    feat(typescript): extend config

--- plugins/typescript.js
+++ plugins/typescript.js
@@ -3,17 +3,45 @@
  */
 module.exports = {
   rules: {
+    '@typescript-eslint/array-type': ['error', { default: 'array' }],
+    '@typescript-eslint/consistent-type-assertions': [
+      'error',
+      {
+        assertionStyle: 'as',
+        objectLiteralTypeAssertions: 'allow-as-parameter',
+      },
+    ],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { prefer: 'type-imports' },
+    ],
+    '@typescript-eslint/method-signature-style': ['error', 'method'],
+    '@typescript-eslint/sort-type-union-intersection-members': ['error'],
+    '@typescript-eslint/unified-signatures': ['error'],
     '@typescript-eslint/member-ordering': [
       'warn',
       {
         default: [
+          // Index signature
+          'signature',
+
+          // Fields
           'public-static-field',
           'protected-static-field',
           'private-static-field',
 
+          'public-decorated-field',
+          'protected-decorated-field',
+          'private-decorated-field',
+
           'public-instance-field',
           'protected-instance-field',
           'private-instance-field',
+
+          'public-abstract-field',
+          'protected-abstract-field',
+          'private-abstract-field',
 
           'public-field',
           'protected-field',
@@ -21,18 +49,35 @@ module.exports = {
 
           'static-field',
           'instance-field',
+          'abstract-field',
+
+          'decorated-field',
 
           'field',
 
+          // Constructors
+          'public-constructor',
+          'protected-constructor',
+          'private-constructor',
+
           'constructor',
 
+          // Methods
           'public-static-method',
           'protected-static-method',
           'private-static-method',
 
+          'public-decorated-method',
+          'protected-decorated-method',
+          'private-decorated-method',
+
           'public-instance-method',
           'protected-instance-method',
           'private-instance-method',
+
+          'public-abstract-method',
+          'protected-abstract-method',
+          'private-abstract-method',
 
           'public-method',
           'protected-method',
@@ -40,9 +85,46 @@ module.exports = {
 
           'static-method',
           'instance-method',
+          'abstract-method',
+
+          'decorated-method',
 
           'method',
         ],
+      },
+    ],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      // {
+      //   "selector": "default",
+      //   "format": ["camelCase"]
+      // },
+      // {
+      //   "selector": "variable",
+      //   "format": ["camelCase", "UPPER_CASE"]
+      // },
+      // {
+      //   "selector": "parameter",
+      //   "format": ["camelCase"],
+      //   "leadingUnderscore": "allow"
+      // },
+      {
+        selector: 'memberLike',
+        modifiers: ['private'],
+        format: ['camelCase'],
+        leadingUnderscore: 'require',
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: true,
+        },
       },
     ],
   },
