0914db5f00e3bb22013445991d765fd6afe3b4c0
Alexey Litovchenko <alex.lit@outlook.com>
2021-04-12 00:44:48 +0300

    chore: preparing for version update

--- plugins/testing-library.js
+++ plugins/testing-library.js
@@ -2,7 +2,7 @@
  * @see [eslint-plugin-testing-library](https://github.com/testing-library/eslint-plugin-testing-library)
  */
 module.exports = {
-  extends: ['plugin:testing-library/recommended', 'plugin:testing-library/vue'],
+  extends: ['plugin:testing-library/vue'],
 
   plugins: ['testing-library'],
 };
