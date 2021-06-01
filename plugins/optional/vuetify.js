40d00d5530527e5bfffa896c43086573a400cbc0
Alexey Litovchenko <alex.lit@outlook.com>
2021-06-01 10:48:45 +0300

    chore: preparing for version update

--- plugins/optional/vuetify.js
+++ plugins/optional/vuetify.js
@@ -2,11 +2,7 @@
  * @see [eslint-plugin-vuetify](https://github.com/vuetifyjs/eslint-plugin-vuetify)
  */
 module.exports = {
-  plugins: ['vuetify'],
+  extends: ['plugin:vuetify/recommended'],
 
-  rules: {
-    'vuetify/grid-unknown-attributes': 'error',
-    'vuetify/no-deprecated-classes': 'error',
-    'vuetify/no-legacy-grid': 'error',
-  },
+  plugins: ['vuetify'],
 };
