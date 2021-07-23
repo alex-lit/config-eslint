129907720516d830af619d8142ecc2e3148ea629
Alexey Litovchenko <alex.lit@outlook.com>
2021-07-23 10:22:39 +0300

    chore: preparing for version update

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,8 +15,6 @@ module.exports = defineConfig({
      */
     'airbnb-base',
 
-    'plugin:@getify/proper-ternary/getify-says',
-
     /* plugins */
     ...[
       './plugins/array-func',
@@ -99,12 +97,9 @@ module.exports = defineConfig({
     parser: '@typescript-eslint/parser',
   },
 
-  plugins: ['@getify/proper-ternary'],
-
   root: true,
 
   rules: {
-    '@getify/proper-ternary/parens': ['error', { ternary: true }],
     'class-methods-use-this': 'off',
     'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
     'grouped-accessor-pairs': ['error', 'getBeforeSet'],
