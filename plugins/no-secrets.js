d60518049bf5266b3275b38ff5879356798b346f
Alexey Litovchenko <alex.lit@outlook.com>
2021-12-24 17:17:11 +0300

    chore: preparing for version update

--- plugins/no-secrets.js
+++ plugins/no-secrets.js
@@ -8,7 +8,7 @@ module.exports = {
     'no-secrets/no-secrets': [
       'error',
       {
-        ignoreContent: ['<', '://', '@example', 'META'],
+        ignoreContent: ['<', '://', '@example', 'META', 'I18N'],
       },
     ],
   },
