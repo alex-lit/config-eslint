aa29d7eb51846689a22ba720c0c9a8ee8f20da66
Alexey Litovchenko <alex.lit@outlook.com>
2023-09-28 14:27:00 +0300

    chore: preparing for version update

--- plugins/more.js
+++ plugins/more.js
@@ -2,6 +2,16 @@
  * @see [eslint-plugin-more](https://github.com/WebbyLab/eslint-plugin-more)
  */
 module.exports = {
+  overrides: [
+    {
+      files: ['**/*.{test,spec}.{js,ts}'],
+
+      rules: {
+        'more/no-numeric-endings-for-variables': 'off',
+      },
+    },
+  ],
+
   plugins: ['more'],
 
   rules: {
